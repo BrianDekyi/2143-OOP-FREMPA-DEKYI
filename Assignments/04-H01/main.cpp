fe982e8f6461c36dc8147e8ad6da290bbbad92e3
BrianDekyi <36304150+BrianDekyi@users.noreply.github.com>
2020-02-11 11:42:13 -0600

    Descriptions of some OOP terminology

---
+++ Assignments/04-H01/main.cpp
@@ -0,0 +1,21 @@
+Name: Brian Frempa-Dekyi
+Date: 02/04/2020
+Class: 2143 OOP
+
+1. Abstraction - It refers to the concept of hiding the complexities of a system from the users of that system.
+2. Attributes - They are data stored inside a class or instance and represent the state or quality of the class or instance.
+3. Class - It is a blueprint for creating objects (a particular data structure), providing initial values for state (member variables or attributes), and implementations of behavior (member functions or methods)
+4. Class variable is any variable declared with the static modifier of which a single copy exists, regardless of how many instances of the class exist.
+5. Composition - It describes a class that references one or more objects of other classes in instance variables.
+6. Encapsulation - It describes the idea of bundling data and methods that work on that data within one unit.
+7. Friends - It is a function that is given the same access as methods to private and protected data.
+8. Inheritance - It is a mechanism in which one class acquires the property of another class.
+9. Instance variable - It is a variable defined in a class (i.e. a member variable), for which each instantiated object of the class has a separate copy, or instance.
+10. Member Variable - It is a variable that is associated with a specific object, and accessible for all its methods (member functions).
+11. Method - It is a procedure associated with a class. It also defines the behavior of the objects that are created from the class.
+12. Multiple Inheritance - It is a feature of some object-oriented computer programming languages in which an object or class can inherit characteristics and features from more than one parent object or parent class.
+13. Object - It is a component of a program that knows how to perform certain actions and how to interact with other elements of the program.
+14. Overloading - It occurs when two or more methods in one class have the same method name but different parameters.
+15. Polymorphism - It refers to a programming language's ability to process objects differently depending on their data type or class. 
+16. Public / Private / Protected - Public means everyone is allowed to access, Private means that only members of the same class are allowed to access, and Protected means that members of subclasses are also allowed.
+17. Virtual - It is a member function that you expect to be redefined in derived classes.
